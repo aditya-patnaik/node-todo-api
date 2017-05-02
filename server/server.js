708cedd5392007b774182113ca6d7223b5e80de0
aditya-patnaik <aditya.narayan.patnaik@gmail.com>
2017-05-02 08:56:50 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectId } = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -27,6 +28,23 @@ app.get('/todos', (req, res) => {
 	}, (err) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if(!ObjectId.isValid(id)){
+		return res.status(404).send();
+	} else{
+		Todo.findById(id).then((todo)=>{
+			if(!todo){
+				res.status(404).send();
+			} else{
+				res.send(todo);
+			}
+		}, (err)=>{
+			res.status(400).send();
+		});
+	}
 });
 
 app.listen(3000, () => {
