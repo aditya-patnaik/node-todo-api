35abb43f8c347bae7898ef1f420b026cbf0bf90b
aditya-patnaik <aditya.narayan.patnaik@gmail.com>
2017-05-06 16:29:41 +0530

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -34,17 +34,16 @@ app.get('/todos/:id', (req, res) => {
 	var id = req.params.id;
 	if(!ObjectId.isValid(id)){
 		return res.status(404).send();
-	} else{
-		Todo.findById(id).then((todo)=>{
-			if(!todo){
-				res.status(404).send();
-			} else{
-				res.send(todo);
-			}
-		}, (err)=>{
-			res.status(400).send();
-		});
-	}
+	} 
+	Todo.findById(id).then((todo)=>{
+		if(!todo){
+			return res.status(404).send();
+		}
+
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
 });
 
 app.listen(3000, () => {
